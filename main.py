c63f8e97cbd93e0110b14a47dff5af55ed5e6060
unammcotter <unammcotter@gmail.com>
2025-02-01 18:41:58 +0000

    Issue - chrome driver not found

--- main.py
+++ main.py
@@ -3,8 +3,29 @@
 #Scapper File test area
 
 import requests
-response = requests.get("https://stats.beepbeep.ie/")
-if response.status_code == 200:
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service                
+
+URL = "https://stats.beepbeep.ie/"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--disable-extensions")  
+chrome_options.add_argument("--disable-gpu")
+ser = Service("chromedriver.exe")
+service = webdriver.ChromeService(executable_path=chromedriver_bin)
+driver = webdriver.Chrome(service=ser,options=chrome_options)
+driver.get(URL)
+print(driver.title)
+
+#element = driver.find_element(By.ID ,"#Id_of_element")
+
+
+page = requests.get(URL)
+if page.status_code == 200:
+    soup = BeautifulSoup(page.content, "html.parser")
     print("Website Accessed Successfully.")
+    results = soup.find(id = "sales-by-segment")
+    #print(results.prettify())
 else:
     print("Issue Accessing Website")
