f3391ee5f075ac7808d0ba730c883123871f3a10
unammcotter <unammcotter@gmail.com>
2025-02-12 15:35:04 +0000

    can now fill in year & month in date filter

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service                
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import Select
 from time import sleep
 import array as arr
 
@@ -22,11 +23,50 @@ def scrape_site (url, YR, MTH, car_make, ENGINE):
     #driver = webdriver.Chrome(service=ser,options=chrome_options)
     driver.get(url)
     sleep(2)
-
     print(driver.title)
 
-    #element = driver.find_element(By.ID ,"#Id_of_element")
+    #access drop down filter menu
+    #find & click on drowp down menu
+    drop_arrow = driver.find_element(By.XPATH,'//*[@id="app"]/div[4]')
+    drop_arrow.click()
+    sleep(5)
 
+    #find & click on date menu
+    date_filter = driver.find_element(By.XPATH,'//*[@id="app"]/div[4]/div[2]/div/div[1]/div[1]/a')
+    date_filter.click()
+    sleep(5)
+
+    #find & click on year
+    #select year
+    year_element = driver.find_element(By.XPATH,'/html/body/div/div[4]/div[2]/div/div[1]/div[1]/div[2]/div[1]/div[1]/div/div/select')
+    year = Select(year_element)
+    #option_list = year.options
+    year.select_by_value('2024')
+    sleep(5)
+
+    year_element2 = driver.find_element(By.XPATH,'/html/body/div/div[4]/div[2]/div/div[1]/div[1]/div[2]/div[1]/div[2]/div/div/select')
+    year2 = Select(year_element2)
+    #option_list = year.options
+    year2.select_by_value('2024')
+    sleep(5)
+
+    #find & click on month
+    #select start month
+    year_element = driver.find_element(By.XPATH,'/html/body/div/div[4]/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[1]/div/div/select')
+    year = Select(year_element)
+    #option_list = year.options
+    year.select_by_visible_text('January')
+    sleep(5)
+
+    #select start month
+    year_element2 = driver.find_element(By.XPATH,'/html/body/div/div[4]/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[2]/div/div/select')
+    year2 = Select(year_element2)
+    #option_list = year.options
+    year2.select_by_visible_text('January')
+    sleep(5)
+
+
+    #element = driver.find_element(By.ID ,"#Id_of_element")
 
     page = requests.get(url)
     #soup = BeautifulSoup(page.content, "html.parser")
@@ -35,10 +75,10 @@ def scrape_site (url, YR, MTH, car_make, ENGINE):
     seg_table = driver.find_element(By.XPATH,'//*[@id="sales-by-segment"]/div[2]/div/div/table')
 
 
+
     if not seg_table:
         print("Segment element not found")
     #print(results.prettify())
-
 
     if page.status_code == 200:
         soup = BeautifulSoup(page.content, "html.parser")
@@ -48,15 +88,17 @@ def scrape_site (url, YR, MTH, car_make, ENGINE):
     else:
         print("Issue Accessing Website")
 
+    return
+
 if __name__ == "__main__":
     url = "https://stats.beepbeep.ie/"
 
-    YR = arr.array('y',[2024,2023,2022])
+    YR = arr.array('i',[2024,2023,2022])
 
     MTH = ["January", "February", "March", "April", "May", "June", "July",
             "August", "September", "October", "November", "December"]
 
-    MTHnum = arr.array('m',[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    MTHnum = arr.array('i',[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
     ENGINE = ["Electric", "Diesel Electric (Hybrid)", "Diesel/Plug-In Electric Hybrid",
             "Petrol Electric (Hybrid)", "Petrol/Plug-In Electric Hybrid"]
